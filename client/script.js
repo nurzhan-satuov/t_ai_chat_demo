468ed284c51dfda8083a1ac222aa11bca041fbe1
Nurzhan Satuov <nsatuov@gmail.com>
2023-03-12 17:53:38 +0800

    Added min length for user id v2

--- client/script.js
+++ client/script.js
@@ -66,7 +66,7 @@ function chatStripe(isAi, value, uniqueId) {
 function basicUserCheck(){
   let user = prompt("Please input your User ID");
   
-  while (!("nurzhanarusayanabay").includes(user) && user.length > 2){
+  while (!("nurzhanarusayanabay").includes(user) && user.length < 3){
     alert("User ID incorrect, please try again");
     console.log("Incorrect username entered: " + user);
     user = prompt("Please input your User ID");
